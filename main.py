62d6102781cd2bfb814328316e37e5aa39fc8681
AlexanderGolys <szyba71830>
2019-06-04 06:37:04 +0200

    done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import os
 import string
 from sklearn.feature_extraction.text import CountVectorizer
@@ -8,60 +8,97 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.pipeline import Pipeline
-from json import loads
-import urllib
+
+
+def answer(ans):
+    if ans[0] == '0':
+        print("renesans")
+    elif ans[0] == '1':
+        print("barok")
+    elif ans[0] == '2':
+        print("oswiecenie")
+    elif ans[0] == '3':
+        print("romantyzm")
+
+
+def add_data(texts, char_nb, data):
+    for i in range(len(texts)):
+        if (texts[i] != '' and texts[i][0] == char_nb) or (len(texts[i]) > 1 and texts[i][1] == char_nb and texts[i][0] == ' '):
+            data.append(texts[i])
+            count = 0
+        elif count < 21:
+            data[-1] += texts[i]
+            count += 1
 
 
 def clean_text(text):
     res = ''
     for char in text:
-        if char not in string.punctuation and char != '0' and char != '1' and char != '2' and char != '/n':
+        if char not in string.punctuation and char != '/n' and char != '*' and char != '-' and char != '«' and char != '»':
             res += char.lower()
-    return res
+    if len(text) > 0 and text[0] == ' ':
+        return res[1:]
+    else:
+        return res
 
 
 def text_process(text):
     return [word for word in text.split()]
 
 
+def main():
+    text = [line.rstrip() + ' ' for line in open('./data_renesans.csv')]
+    text2 = [line.rstrip() + ' ' for line in open('./data_Oswiecenie_300.txt')]
+    text3 = [line.rstrip() + ' ' for line in open('./data_Romantyzm_300.txt')]  #read data from files
+    data = [text[0]]
 
-text = [line.rstrip() + ' ' for line in open('./data_renesans.csv')]
-data = [text[0]]
+    count = 0
+    for i in range(len(text)):   #divide data to singular texts
+        if text[i] != '' and (text[i][0] == '0' or text[i][0] == '1'):
+            data.append(text[i])
+            count = 0
+        elif count < 21:
+            data[-1] += text[i]
+            count += 1
 
-for i in range(len(text)):
-    if text[i] != '' and (text[i][0] == '0' or text[i][0] == '1' or text[i][0] == '2'
-    or text[i][0] == '3' or text[i][0] == '4' or text[i][0] == '5' or text[i][0] == '6'):
-        data.append(text[i])
-    else:
-        data[-1] += text[i]
+    add_data(text2, '2', data)  # add data from last 2 epochs
+    data = data[:-80]  # rebalances number of samples
+    add_data(text3, '3', data)
+    data = data[:-60]
 
-better_data = []
-for text in data:
-    better_data.append([text[0], text[2:]])
-better_data = better_data[1:]
+    clean_data = [clean_text(line) for line in data]  # cleaning texts
+    better_data = []
+    for text in clean_data:  # divide data into epoch number and text
+        better_data.append([text[0], text[2:]])
+    better_data = better_data[1:]  # delete repetition
 
-for i in range(len(better_data)):
-    better_data[i][1] = clean_text(better_data[i][1])
+    data = pd.DataFrame(np.array(better_data), columns=['a', 'b'])  # creating pandas dataframe from data
+    text_train, text_test, epoch_train, epoch_test = train_test_split(data['b'], data['a'], test_size=0.3)  # split data
 
-data = pd.DataFrame(np.array(better_data), columns=['a', 'b'])
-print(data[data['a'] == '3'])
-text_train, text_test, epoch_train, epoch_test = train_test_split(data['b'], data['a'], test_size=0.2)
-
-pipeline = Pipeline([
-   ('bow', CountVectorizer(analyzer=text_process)),
-    ('tfidf', TfidfTransformer()),
-    ('classifier', MultinomialNB()),
-])
-
-pipeline.fit(text_train, epoch_train)
-
-pred = pipeline.predict(text_test)
-
-print('renesans: ', data['a'][data['a'] == '0'].count())
-print('barok: ', data['a'][data['a'] == '1'].count())
-
-print(classification_report(pred, epoch_test))
+    print(data.describe(), data.a.unique())
+    print('renesans: ', data['a'][data['a'] == '0'].count())
+    print('barok: ', data['a'][data['a'] == '1'].count())
+    print('oswiecenie: ', data['a'][data['a'] == '2'].count())
+    print('romantyzm: ', data['a'][data['a'] == '3'].count())
 
 
-# oswiecenie = loads(urllib.request.urlopen("https://wolnelektury.pl/katalog/epoka/oswiecenie/").read().decode('utf-8'))
-# print(oswiecenie)
+    pipeline = Pipeline([  # create classifier
+       ('bow', CountVectorizer(analyzer=text_process)),  # create bucket of words
+        ('tfidf', TfidfTransformer()),  # create tf-idf from bow
+        ('classifier', MultinomialNB()),  # create Naive Bayes
+    ])
+
+    pipeline.fit(text_train, epoch_train)  # feed classifier with data
+
+    pred = pipeline.predict(text_test)  # check accuracy on test data
+    print(classification_report(pred, epoch_test))  # print report from checking
+    text = ''
+    for line in open('./text.txt'):  # read text to be classify
+        text += line
+    text = clean_text(text)  # clean text to be classify
+    print(text)
+    answer(pipeline.predict([text]))  # print answer
+
+
+main()
+
